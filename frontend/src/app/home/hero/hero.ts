669d8c1af92b23824cf65085131097a267011d94
salma447105 <83430323+salma447105@users.noreply.github.com>
2025-11-02 01:14:36 +0200

    Delete frontend/src directory --deprecated

--- frontend/src/app/home/hero/hero.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-hero',
-  standalone: true,
-  templateUrl: './hero.html',
-})
-export class Hero {}
--- frontend/src/app/shop/shop-hero/shop-hero.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-shop-hero',
-  standalone: true,
-  templateUrl: './shop-hero.html',
-})
-export class ShopHero {}
