31279f08eec7e9c1f01095462d31d02619fcb482
salma447105 <salma447105@gmail.com>
2025-11-01 21:57:50 +0200

    deployment mod + navbar cart update

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,22 +1,37 @@
-import { Component , OnInit } from '@angular/core';
+import { Component, OnInit, effect, computed, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FavoritesService } from '../services/favorites';
+import { CartService } from '../services/cart/cart.service';
+
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   imports: [RouterModule, CommonModule],
   templateUrl: './navbar.html',
-  styleUrls: ['./navbar.css']  
+  styleUrls: ['./navbar.css']
 })
-  
-export class Navbar implements OnInit {
 
+export class Navbar implements OnInit {
   favoritesCount: number = 0;
-  constructor(private favoritesService: FavoritesService) {} 
+  cartItemCount: number = 0;
+
+  constructor(
+    private favoritesService: FavoritesService,
+    private cartService: CartService
+  ) {
+    // Set up cart count effect in constructor
+    effect(() => {
+      const count = this.cartService.getItemCount();
+      console.log('Cart count updated:', count);
+      this.cartItemCount = count;
+    });
+  }
 
   ngOnInit() {
-      this.favoritesCount = this.favoritesService.getFavoritesCount();
-      this.favoritesService.getFavoritesObservable().subscribe(favorites => {
+    // Initialize favorites count
+    this.favoritesCount = this.favoritesService.getFavoritesCount();
+    this.favoritesService.getFavoritesObservable().subscribe(favorites => {
       this.favoritesCount = favorites.length;
     });
   }
